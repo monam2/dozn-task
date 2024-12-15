ec0699a8cff77c2f3dd185485d35f654ea6d61c8
monam2 <kangcw0107@gmail.com>
2024-12-16 05:43:24 +0900

    #22 [FEAT] 쿼리 키 관리

--- src/react-query/keys.ts
+++ src/react-query/keys.ts
@@ -1,1 +1,8 @@
-// 쿼리키 관리 파일
+/**
+ * 쿼리키 관리 파일
+ */
+export const keys = {
+  all: ["dozn"] as const,
+  list: (pageIdx: string, pageSize?: string) => [...keys.all, "list", pageIdx, pageSize],
+  scrp: (mdulCustCd: string, apiCd: string) => [...keys.all, "scrp", mdulCustCd, apiCd],
+};
