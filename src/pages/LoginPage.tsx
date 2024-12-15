2bb9a3e73e76bc369f5775a914a9876ac3da0ada
monam2 <kangcw0107@gmail.com>
2024-12-16 00:28:35 +0900

    #18 [DESIGN] 로그인 페이지 디자인 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,7 +2,7 @@ import LoginForm from "./../components/login/LoginForm";
 
 export default function LoginPage() {
   return (
-    <div className="flex w-full h-[900px] justify-center items-start bg-background">
+    <div className="flex w-full min-h-[900px] justify-center items-start">
       <LoginForm />
     </div>
   );
