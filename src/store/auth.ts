12c2d32a6ccdb29de077259f094e4c70e3673fe3
monam2 <kangcw0107@gmail.com>
2024-12-15 23:49:39 +0900

    #16 [CHORE] auth store 주석 추가

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,6 +1,11 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
+/**
+ * 로그인 관련 정보를 전역으로 관리하는 store
+ *
+ * Auth 타입 : AccessTokenPayload에서 필요한 값들만 사용
+ */
 type Auth = Pick<AccessTokenPayload, "apiKey" | "exp" | "identification">;
 
 interface AuthStore {
