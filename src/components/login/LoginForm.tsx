7090cc7ec86645f8b0bb0c0703a04861548600f8
monam2 <kangcw0107@gmail.com>
2024-12-15 21:39:49 +0900

    #13 [FEAT] 로그인 페이지

--- src/components/login/LoginForm.tsx
+++ src/components/login/LoginForm.tsx
@@ -1,49 +1,52 @@
-import { FormEvent } from "react";
-import BaseInput from "../common/BaseInput";
 import BaseButton from "../common/BaseButton";
+import useLoginForm from "./useLoginForm";
+import LabeledInput from "./../common/LabeledInput";
 
 export default function LoginForm() {
-  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("first");
-  };
+  const {
+    admUserId,
+    userPw,
+    errors,
+    isLoading,
+    handleAdmUserIdChange,
+    handleUserPwChange,
+    handleSubmit,
+  } = useLoginForm();
 
   return (
     <form
-      onSubmit={(e) => submitHandler(e)}
+      onSubmit={(e) => handleSubmit(e)}
       className="flex flex-col min-w-[400px] h-4/5 justify-center gap-12 pt-[150px] pb-[100px]"
     >
-      <h1 className="text-heading-1 text-center font-bold text-black">
-        로그인
-      </h1>
+      <h1 className="text-heading-1 text-center font-bold">로그인</h1>
       <div className="flex flex-col gap-6">
-        <div className="flex flex-col gap-3">
-          <label
-            htmlFor="id"
-            className="text-content-2 font-semibold text-black"
-          >
-            아이디
-          </label>
-          <BaseInput
-            placeholder="아이디 입력"
-            bottomLabel="아이디를 확인해주세요."
-          />
-        </div>
-        <div className="flex flex-col gap-3">
-          <label
-            htmlFor="id"
-            className="text-content-2 font-semibold text-black"
-          >
-            비밀번호
-          </label>
-          <BaseInput
-            type="password"
-            placeholder="비밀번호 입력"
-            bottomLabel="비밀번호가 일치하지 않습니다."
-          />
-        </div>
+        <LabeledInput
+          id="admUserId"
+          label="아이디"
+          value={admUserId}
+          onChange={handleAdmUserIdChange}
+          error={errors.admUserId}
+          placeholder="아이디 입력"
+          bottomLabel="아이디를 다시 입력해주세요."
+        />
+        <LabeledInput
+          id="password"
+          label="비밀번호"
+          type="password"
+          value={userPw}
+          onChange={handleUserPwChange}
+          error={errors.userPw}
+          placeholder="비밀번호 입력"
+          bottomLabel="비밀번호를 다시 입력해주세요."
+        />
       </div>
-      <BaseButton color="primary" size="full">
+      <BaseButton
+        type="submit"
+        color="primary"
+        size="full"
+        isLoading={isLoading}
+        disabled={isLoading}
+      >
         로그인
       </BaseButton>
     </form>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import LoginForm from "./../components/login/LoginForm";
 
 export default function LoginPage() {
